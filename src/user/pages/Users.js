fab595df0619ab26ab2157f409b1de4c0c18c169
mks-94 <muralikrishna9424@gmail.com>
2020-08-17 22:03:46 +0530

    Implemented nav and other components

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React from "react";
+
+import UserList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Murali Krishna",
+      image:
+        "https://image.shutterstock.com/image-photo/colombo-national-museum-sri-lanka-600w-1749179987.jpg",
+      places: 3,
+    },
+  ];
+  return <UserList items={USERS} />;
 };
 
 export default Users;
